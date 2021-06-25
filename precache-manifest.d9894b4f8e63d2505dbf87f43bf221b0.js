0d67c0ff26a4aa6a2a234c936d3528511845cac1
Shinigami <Clearlyio@protonmail.com>
2021-06-25 10:30:12 -0700

    Update v2

---
+++ precache-manifest.d9894b4f8e63d2505dbf87f43bf221b0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69c890fd69972cb6ef8",
+    "url": "/css/apitool.b346a46d.css"
+  },
+  {
+    "revision": "aef18d9279c927545afd",
+    "url": "/css/app.6e50b03d.css"
+  },
+  {
+    "revision": "a96adc9a61465699887c",
+    "url": "/css/biography.799ba10e.css"
+  },
+  {
+    "revision": "1d56acb7376a110975f2",
+    "url": "/css/chunk-vendors.91427cc0.css"
+  },
+  {
+    "revision": "c81fb9a03655dd6e7c9c",
+    "url": "/css/lang.249841e9.css"
+  },
+  {
+    "revision": "f3da1795eb985fe8d8ff",
+    "url": "/css/projects.f39c7679.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "6bd22b12484dcf5bbbf624b10ab4b91d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e69c890fd69972cb6ef8",
+    "url": "/js/apitool.531205e5.js"
+  },
+  {
+    "revision": "5b5f88edf273e793f268",
+    "url": "/js/apitool~projects.287cf8d3.js"
+  },
+  {
+    "revision": "aef18d9279c927545afd",
+    "url": "/js/app.695bc53c.js"
+  },
+  {
+    "revision": "a96adc9a61465699887c",
+    "url": "/js/biography.ea4bc9e8.js"
+  },
+  {
+    "revision": "1d56acb7376a110975f2",
+    "url": "/js/chunk-vendors.ce3537dc.js"
+  },
+  {
+    "revision": "c81fb9a03655dd6e7c9c",
+    "url": "/js/lang.3045c0e8.js"
+  },
+  {
+    "revision": "f3da1795eb985fe8d8ff",
+    "url": "/js/projects.08d185e0.js"
+  },
+  {
+    "revision": "2816051fccd98844d43b376d20be0ac1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
